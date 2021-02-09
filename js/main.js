2dfa2235b7ffc0bc71582c9dcd00ea1f5e21037d
Nicola <nicolusso@gmail.com>
2021-02-09 15:47:00 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-	//Code
-});
+const app = new Vue ( {
+  el: '#app',
+  data: {
+    listCds: [],
+  },
+
+  mounted(){
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(result => {
+      this.listCds = result.data.response;
+      console.log(result.data.response);
+    });
+
+  }
+})
